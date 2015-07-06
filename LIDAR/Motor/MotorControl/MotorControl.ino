e434464bdb3bc03f4e6cd252525846f034ffd983
Lyndon Shi <lyndrive@gmail.com>
2015-07-05 22:55:48 -0400

    Some motor updates

--- LIDAR/Motor/MotorControl/MotorControl.ino
+++ LIDAR/Motor/MotorControl/MotorControl.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(8,INPUT);
-}
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created      
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(4,OUTPUT); 
+} 
+ 
+ 
+void loop() 
+{ 
+  for(pos = 0; pos < 180; pos += 1){  // goes from 0 degrees to 180 degrees in steps of 1 degree 
+    digitalWrite(4,HIGH);
+    myservo.write(pos);               // tell servo to go to position in variable 'pos' 
+    delay(15);                        // waits 15ms for the servo to reach the position
+    digitalWrite(4,LOW); 
+  } 
 }
